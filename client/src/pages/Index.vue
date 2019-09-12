d59a6478b75f2f5de3da1e494982b96ab5146962
ruhlelana <ruhlelana@gmail.com>
2019-09-12 15:22:20 +0700

    fonts

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,10 +1,12 @@
 <template>
   <q-page class="flex flex-center q-pa-md">
-    <q-img src="./statics/logoVery.png" />
-    <h3 class="text-bold q-pa-sm">Digital creative agency
+    <q-img src="./statics/logoVery.png" style="max-height: 10em;max-width:20em;" />
+    <div class="q-pa-sm">
+    <h3 class="text-bold my-font">Digital creative agency
       <br>Brand new</h3>
       <p>Deliverables: Apps, webapps, contents, cool shit.<br>
       Magicians: programmers, designers, animators, artists.</p>
+    </div>
   </q-page>
 </template>
 
